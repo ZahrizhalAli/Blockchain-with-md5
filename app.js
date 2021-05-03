28544059010a44385acedb96fdecb5ed615b5970
zahrizhalali29 <zzahrizhal.ali@gmail.com>
2021-05-03 16:59:10 +0800

    Attempt1: Still on progress..

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const ejs = require('ejs');
+const md5 = require('md5');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
+app.get("/", function(req,res){
+  res.render("index");
+})
+
+app.listen(3000, function(req,res){
+  console.log("RUNS ON PORT 3000")
+})
